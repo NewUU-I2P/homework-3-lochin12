6d799a1b223c8faf0171f9361ce1bf45bafb4621
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-10 10:09:54 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,7 +1,7 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
-    float result = (i > j) ? ((i > k) ? i : k) : ((j > k) ? j : k);
+ result = (i > j) ? ((i > k) ? i : k) : ((j > k) ? j : k);
 
     return result;
 }
