8d6b7cc65cac80ae6a42dc51608d8c71aec3c05e
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 20:33:49 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,13 +1,13 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-if (consumed_water<=30){
-    cost= (consumed_water*0.4) +13;}
-if (consumed_water>30 and consumed_water<=50){
-        cost= (consumed_water-30)*0.12 + 30*0.4 +13;}
-if (consumed_water>50 and consumed_water<=60){
-    cost = (consumed_water -50)*1.4+ 30*0.4 + 20*0.12+13;}
-if (consumed_water>60)
-     cost= (consumed_water-60)*1.5 + 1.4*10 +20*0.12 +30*0.4+13;
+if (consumed_water<=30)
+    cost= (consumed_water*0.4) +13;
+    if (consumed_water>30 and consumed_water<=50)
+        cost= (consumed_water-30)*0.12 + 30*0.4 +13;
+        if (consumed_water>50 and consumed_water<=60)
+            cost = (consumed_water -50)*1.4+ 30*0.4 + 20*0.12+13;
+            if (consumed_water>60)
+                 cost= (consumed_water-60)*1.5 + 1.4*10 +20*0.12 +30*0.4+13;
     return cost;
 }
