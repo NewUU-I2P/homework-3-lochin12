d0402006b956d8e9ee9f56e98c6d079329577d46
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 23:34:04 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,35 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
+ switch (operation) {
+        case '+':
+            result = x + y;
+            break;
+        case '-':
+            result = x - y;
+            break;
+        case '*':
+            result = x * y;
+            break;
+        case '/':
+            // Check for division by zero
+            if (y != 0) {
+                result = x / y;
+            } else {
+                std::cerr << "Error: Division by zero!" << std::endl;
+                // Set result to a special value or handle the error as needed
+                // For simplicity, we set the result to 0.0 in case of division by zero
+                result = 0.0;
+            }
+            break;
+        default:
+            std::cerr << "Error: Invalid operation!" << std::endl;
+            // Set result to a special value or handle the error as needed
+            // For simplicity, we set the result to 0.0 in case of an invalid operation
+            result = 0.0;
+            break;
+    }
+
 
    return result;
 }
