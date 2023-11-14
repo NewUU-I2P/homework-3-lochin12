14dfb83bb4516ab9e64ff9ce24e070c03665485c
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 23:45:32 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,28 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-
+ switch (operation) {
+        case '+':
+            result = x + y;
+            break;
+        case '-':
+            result = x - y;
+            break;
+        case '*':
+            result = x * y;
+            break;
+        case '/':
+            if (y != 0) {
+                result = x / y;
+            } else {
+                std::cerr << "Error: Division by zero!" << std::endl;
+                result = 0.0;
+            }
+            break;
+        default:
+            std::cerr << "Error: Invalid operation!" << std::endl;
+            result = 0.0;
+            break;
+    }
    return result;
 }
