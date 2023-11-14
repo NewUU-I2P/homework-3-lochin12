291e3bc6480ef114104c397f9a1052816cc81d0d
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 20:47:54 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,18 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-
+if (S='M')
+{
+    if ( height<1.7){cout<<"Short"; }
+    else if (height >= 1.7 and height<1.85){cout << "Normal";}
+    else {cout << "Tall"}
+}
+else if (S='W')
+{ if ( height<1.6){cout<<"Short"; }
+    else if (height >= 1.6 and height<1.75){cout << "Normal";}
+    else {cout << "Tall"}
+}
+    
     // use return to return your result
     // make use of control flow statements
 }
