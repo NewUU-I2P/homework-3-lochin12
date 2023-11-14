500e45ca89bf95418d156a57841e6ea870009139
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 21:53:50 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,17 +1,17 @@
 #include <string>
 
-float problemSolution3(float height, char S){
+string problemSolution3(float height, char S){
     // write your code here
 if (S=='M')
 {
-    if ( height<1.7){std::cout<<"Short"; }
-    else if (height >= 1.7 and height<1.85){std::cout << "Normal";}
-    else {std::cout << "Tall";}
+    if ( height<1.7){return "Short"; }
+    else if (height >= 1.7 and height<1.85){return "Normal";}
+    else {return "Tall";}
 }
 else if (S=='W')
-{ if ( height<1.6){std::cout<<"Short"; }
-    else if (height >= 1.6 and height<1.75){std::cout << "Normal";}
-    else {std::cout << "Tall";}
+{ if ( height<1.6){return "Short"; }
+    else if (height >= 1.6 and height<1.75){return "Normal";}
+    else {return "Tall";}
 }   
 return 0;
 }
