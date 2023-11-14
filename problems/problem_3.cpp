a7c0983893ba1d454190436be1a141152899adc8
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 21:44:57 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,8 +2,6 @@
 
 float problemSolution3(float height, char S); 
     // write your code here
-    float height;
-    char S;
 if (S=='M')
 {
     if ( height<1.7){std::cout<<"Short"; }
@@ -14,5 +12,6 @@ else if (S=='W')
 { if ( height<1.6){std::cout<<"Short"; }
     else if (height >= 1.6 and height<1.75){std::cout << "Normal";}
     else {std::cout << "Tall";}
-}    
+}   
+return 0;
 }
