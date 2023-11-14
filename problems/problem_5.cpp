34f2cbab7a70502453255a21521826e7af8adb7b
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 23:40:44 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,35 +1,47 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
- switch (operation) {
-        case '+':
-            result = x + y;
-            break;
-        case '-':
-            result = x - y;
-            break;
-        case '*':
-            result = x * y;
-            break;
-        case '/':
-            // Check for division by zero
-            if (y != 0) {
-                result = x / y;
-            } else {
-                std::cerr << "Error: Division by zero!" << std::endl;
-                // Set result to a special value or handle the error as needed
-                // For simplicity, we set the result to 0.0 in case of division by zero
-                result = 0.0;
-            }
-            break;
-        default:
-            std::cerr << "Error: Invalid operation!" << std::endl;
-            // Set result to a special value or handle the error as needed
-            // For simplicity, we set the result to 0.0 in case of an invalid operation
-            result = 0.0;
-            break;
-    }
+std::string macX = std::to_string(static_cast<int>(x));
+    std::string macY = std::to_string(static_cast<int>(y));
+
+    auto getMacAddressType = [](const std::string& macAddress) {
+        std::istringstream ss(macAddress);
+        int firstOctet;
+
+       
+        ss >> std::hex >> firstOctet;
+
+ 
+        if (macAddress == "FF:FF:FF:FF:FF:FF") {
+            return "Broadcast";
+        }
+
+     
+        if (firstOctet % 2 == 0) {
+            return "Unicast";
+        }
+
+        
+        if (firstOctet % 2 != 0) {
+            return "Multicast";
+        }
+
+    
+        return "Unknown";
+    };
 
 
+    std::string typeX = getMacAddressType(macX);
+    std::string typeY = getMacAddressType(macY);
+
+    // Display the MAC address types
+    std::cout << "MAC Address Type for x: " << typeX << std::endl;
+    std::cout << "MAC Address Type for y: " << typeY << std::endl;
+
+    // Placeholder for the actual operation logic based on MAC address types
+    // Modify this part according to your specific requirements
+
+    // For now, just return the sum of x and y
+    result = x + y;
    return result;
 }
