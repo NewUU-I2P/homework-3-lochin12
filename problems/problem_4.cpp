a1554e59bfb2e116c75da795100a08c369efcd91
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 23:43:46 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -4,25 +4,16 @@
 std::string problemSolution4(const std::string &macAddress) {
     std::istringstream ss(macAddress);
     int firstOctet;
-
-    // Read the first octet from the MAC address
     ss >> std::hex >> firstOctet;
 
-    // Check for Broadcast address
     if (macAddress == "FF:FF:FF:FF:FF:FF") {
         return "Broadcast";
     }
-
-    // Check for Unicast address
     if (firstOctet % 2 == 0) {
         return "Unicast";
     }
-
-    // Check for Multicast address
     if (firstOctet % 2 != 0) {
         return "Multicast";
     }
-
-    // Default case (should not reach here in a valid scenario)
     return "Unknown";
 }
