8aa94d43dbc1413a01aea34403e8461efb4f840c
Lochin <145972165+lochin12@users.noreply.github.com>
2023-11-14 23:52:42 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-float problemSolution5(float x, float y, char operation) {
-   float result;
-   // write your code here
- switch (operation) {
+int main(){
+float x,y;
+char operation;
+    float result;
+std::cin >> x>> operation >>y;
+    // Perform the specified arithmetic operation
+    switch (operation) {
         case '+':
             result = x + y;
             break;
@@ -13,17 +16,21 @@ float problemSolution5(float x, float y, char operation) {
             result = x * y;
             break;
         case '/':
+            // Check for division by zero
             if (y != 0) {
                 result = x / y;
             } else {
                 std::cerr << "Error: Division by zero!" << std::endl;
+                // Set result to a special value or handle the error as needed
                 result = 0.0;
             }
             break;
         default:
             std::cerr << "Error: Invalid operation!" << std::endl;
+            // Set result to a special value or handle the error as needed
             result = 0.0;
             break;
     }
-   return result;
+
+    return result;
 }
